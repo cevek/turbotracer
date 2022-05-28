d861d1e6864ca42128ccf922bcabc0efbf712107
cevek <x.cevek@gmail.com>
2022-05-28 21:58:22 +0300

    Fix chrome location

--- web.ts
+++ web.ts
@@ -1,12 +1,17 @@
 import {execSync, spawnSync} from 'child_process';
 import {mkdirSync} from 'fs';
 import {tmpdir} from 'os';
+import {chromeLocation} from './chromeLocations';
 import {openHTML} from './openHTML';
 import {parseTurboFiles} from './parseTurboFiles';
 import {removeTurboFiles} from './utils';
 
-const chromeLocation = require('chrome-location') as string;
+const chromeLoc = chromeLocation();
 const url = process.argv[process.argv.length - 1];
+if (!chromeLoc) {
+    console.log('Chrome is not found in your system');
+    process.exit();
+}
 if (url.includes('turbotracer')) throw new Error('You need to specify url');
 const profileDir = tmpdir() + '/turbotracer/';
 try {
@@ -16,7 +21,7 @@ try {
 // console.log(chromeLocation);
 removeTurboFiles('./');
 const output = execSync(
-    `"${chromeLocation}" --js-flags="--trace-turbo --log-code --allow-natives-syntax"  --user-data-dir=${profileDir} --profile-directory="turbotracer" --no-sandbox "${url}"`,
+    `"${chromeLoc}" --js-flags="--trace-turbo --log-code --allow-natives-syntax"  --user-data-dir=${profileDir} --profile-directory="turbotracer" --no-sandbox "${url}"`,
 );
 console.log(output.toString());
 
